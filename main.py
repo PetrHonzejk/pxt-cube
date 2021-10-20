8e5f6fd224d927c29c62b01d9fd8bd8b896c3ffb
Petr Honzejk <91247580+PetrHonzejk@users.noreply.github.com>
2021-10-20 21:44:04 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,135 @@
-def on_forever():
-    if enabled:
-        basic.show_icon(IconNames.YES)
-    else:
-        basic.show_icon(IconNames.NO)
-basic.forever(on_forever)
+def heartbeat():
+    pass
+basic.forever(heartbeat)
 
 enabled = False
+sides = 6
 
 def on_button_pressed_a():
     global enabled
     enabled = True
+    basic.show_icon(IconNames.YES)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def shake():
-    
+def on_logo_up():
+    global sides
+    if sides == 6:
+        sides = 10
+        basic.show_leds("""
+                            # . . . #
+                            . # . # .
+                            # . . . #
+                            . # . # .
+                            # . . . #
+                            """)
+    else:
+        sides = 6
+        basic.show_leds("""
+                            # . . . #
+                            . . . . .
+                            # . . . #
+                            . . . . .
+                            # . . . #
+                            """)
+input.on_gesture(Gesture.LOGO_UP, on_logo_up)
+
+def on_gesture_shake():
+    global enabled
+    global sides
+    if enabled:
+        number = randint(1, sides)
+        if number == 1:
+            basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            """)
+        if number == 2:
+            basic.show_leds("""
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            """)
+        if number == 3:
+            basic.show_leds("""
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            """)
+        if number == 4:
+            basic.show_leds("""
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            """)
+        if number == 5:
+            basic.show_leds("""
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            """)
+        if number == 6:
+            basic.show_leds("""
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            """)
+        if number == 7:
+            basic.show_leds("""
+            # . . . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . . . #
+            """)
+        if number == 8:
+            basic.show_leds("""
+            # . . . #
+            . . # . .
+            # . . . #
+            . . # . .
+            # . . . #
+            """)
+        if number == 9:
+            basic.show_leds("""
+            # . # . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . # . #
+            """)
+        if number == 10:
+            basic.show_leds("""
+            # . . . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . . . #
+            """)
+        for a in range(number):
+            music.play_tone(Note.C, music.beat(0.5))
+            basic.pause(100)
+        enabled = False
+        basic.pause(1000)
+        basic.show_leds("""
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        """)
+    else:
+        basic.show_icon(IconNames.NO)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,13 +1,157 @@
-basic.forever(function on_forever() {
+basic.forever(function heartbeat() {
+    
+})
+let enabled = false
+let sides = 6
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    enabled = true
+    basic.showIcon(IconNames.Yes)
+})
+input.onGesture(Gesture.LogoUp, function on_logo_up() {
+    
+    if (sides == 6) {
+        sides = 10
+        basic.showLeds(`
+                            # . . . #
+                            . # . # .
+                            # . . . #
+                            . # . # .
+                            # . . . #
+                            `)
+    } else {
+        sides = 6
+        basic.showLeds(`
+                            # . . . #
+                            . . . . .
+                            # . . . #
+                            . . . . .
+                            # . . . #
+                            `)
+    }
+    
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    let number: number;
+    
+    
     if (enabled) {
-        basic.showIcon(IconNames.Yes)
+        number = randint(1, sides)
+        if (number == 1) {
+            basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        }
+        
+        if (number == 2) {
+            basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+        }
+        
+        if (number == 3) {
+            basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+        }
+        
+        if (number == 4) {
+            basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        }
+        
+        if (number == 5) {
+            basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+        }
+        
+        if (number == 6) {
+            basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+        }
+        
+        if (number == 7) {
+            basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . . . #
+            `)
+        }
+        
+        if (number == 8) {
+            basic.showLeds(`
+            # . . . #
+            . . # . .
+            # . . . #
+            . . # . .
+            # . . . #
+            `)
+        }
+        
+        if (number == 9) {
+            basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . # . #
+            `)
+        }
+        
+        if (number == 10) {
+            basic.showLeds(`
+            # . . . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . . . #
+            `)
+        }
+        
+        for (let a = 0; a < number; a++) {
+            music.playTone(Note.C, music.beat(0.5))
+            basic.pause(100)
+        }
+        enabled = false
+        basic.pause(1000)
+        basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
     } else {
         basic.showIcon(IconNames.No)
     }
     
-})
-let enabled = false
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    enabled = true
 })
